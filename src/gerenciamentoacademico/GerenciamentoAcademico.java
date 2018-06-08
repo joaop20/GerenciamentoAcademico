98afa5785cfffadbfdf156ea572b1957470aac9a
joaop20 <joao.psilva98@gmail.com>
2018-06-08 20:36:09 -0300

    Initial commit

---
+++ src/gerenciamentoacademico/GerenciamentoAcademico.java
@@ -0,0 +1,9 @@
+package gerenciamentoacademico;
+
+public class GerenciamentoAcademico {
+
+    public static void main(String[] args) {
+        //Roda a aplicacao
+        new frmPrincipal().setVisible(true);
+    }
+}
